e3a10c4940f9de323f290d7b2d1ad965c5686b8b
Zaire Ali <zaireali649@gmail.com>
2022-12-09 01:56:22 +0000

    repetition

--- repetition.py
+++ repetition.py
@@ -17,3 +17,25 @@ for i in range(0,20):
     number = random.randint(0,100)
     if(number > 50):
         print(number)
+
+print("")
+
+objs = ["apple", "banana", "cherry", "lemon"]
+
+for obj in objs:
+    if("r" in obj):
+        print(obj)
+        break
+
+number = random.randint(0,100)
+
+counter = 0
+
+while(number > 10):
+    if(counter >= 2000):
+        break
+    
+    number = random.randint(0,100000)
+    counter += 1 # counter = counter + 1
+    
+print(counter, number)
